b338608c996422a16be58e4069f78dbb4812614d
Norman Craig <normancraig2@gmail.com>
2015-07-23 21:41:25 -0400

    Initial Commit

---
+++ myScripts.js
@@ -0,0 +1,11 @@
+$('#sr').click (function () {
+	$("#sr p").slideToggle();
+});
+
+$('#chuck').click (function () {
+	$("#chuck p").slideToggle();
+});
+
+$('#anderson').click (function () {
+	$("#anderson p").slideToggle();
+});
